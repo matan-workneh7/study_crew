d21eb0be71030c372c6e537fdbf061fdd92d4b5d
matan-workneh7 <matanworkneh77@gmail.com>
2025-07-26 19:10:53 +0300

    added model to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,19 @@
 class User < ApplicationRecord
     has_secure_password
+    
+    has_many :assistant_course, foreign_key: :assistant_id
+
+    validates :name, presence: true
+    validates :email, uniqueness:true
+    validates :role, presence: true, inclusion: { in: %w[ user assistant ] }
+    validates :academic_year, presence: true
+    validates :password, presence:true, length {greater_than_or_equal_to: 8}
+    
+    def assistant?
+        role == "assistant"
+    end
+
+    def user?
+        role == "user"
+    end
 end
