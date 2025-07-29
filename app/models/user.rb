debe659644eb8d0c81a20a1d4ab7f25c4cf914f7
matan-workneh7 <matanworkneh77@gmail.com>
2025-07-29 10:20:04 +0300

    added the necessary models to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,25 @@
 class User < ApplicationRecord
     has_secure_password
     
-    has_many :assistant_course, foreign_key: :assistant_id
+    #used to define the assistant and their courses
+    has_many :assistant_courses, foreign_key: :assistant_id
+    has_many :courses, through: :assistant_courses, source: :course
+    
 
+    #used to define the connections of a user(learner) and its assistant
+    has_many :connections, dependent: :destroy
+    has_many :assistants, through: :connections, source: :assistant
+
+    #used to define the connections of a user(learner) and its assistant
+    has_many :received_connections, class_name:'Connection', foreign_key: :assistant_id, dependent: :destroy
+    has_many :learners, through: :received_connections, source: :user
+    
+    
     validates :name, presence: true
     validates :email, uniqueness:true
     validates :role, presence: true, inclusion: { in: %w[ user assistant ] }
     validates :academic_year, presence: true
-    validates :password, presence:true, length {greater_than_or_equal_to: 8}
+    validates :password, presence:true, length: { minimum: 8 }
     
     def assistant?
         role == "assistant"
