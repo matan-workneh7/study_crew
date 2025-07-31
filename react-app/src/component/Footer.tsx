b583a0e6069ae6a7cdcfbfc3b9deded5c2454363
Matan Workneh <127529931+matan-workneh7@users.noreply.github.com>
2025-07-31 15:18:22 +0300

    Delete react-app directory

--- react-app/src/component/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer: React.FC = () => (
-  <footer className="footer">
-    <p>&copy; {new Date().getFullYear()} My App. All rights reserved.</p>
-  </footer>
-);
-
-export default Footer;
